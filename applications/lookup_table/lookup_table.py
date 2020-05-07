b7a3065da534e8cb8c9c4638850f4d91dd4d183c
Moses A. Samuel <mosamuel98@gmail.com>
2020-05-07 14:03:24 -0700

    solve lookup_table algo

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,14 +1,20 @@
 import math
 import random
 
-def slowfun(x, y):
-    # TODO: Modify to produce the same results, but much faster
-    v = math.pow(x, y)
-    v = math.factorial(v)
-    v //= (x + y)
-    v %= 982451653
+def slowfun():
+    cache = {}
+    def fasterfun(x, y):
+        # TODO: Modify to produce the same results, but much faster
+        if str(x + y) not in cache:
+            v  = math.pow(x, y)
+            v = math.factorial(v)
+            v //= (x + y)
+            v %= 982451653
+            cache[str(x + y)] = v
 
-    return v
+        return cache[str(x + y)] 
+    
+    return fasterfun
 
 
 # Do not modify below this line!
@@ -16,4 +22,6 @@ def slowfun(x, y):
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    fasterfun = slowfun()
+
+    print(f'{i}: {x},{y}: {fasterfun(x, y)}')
